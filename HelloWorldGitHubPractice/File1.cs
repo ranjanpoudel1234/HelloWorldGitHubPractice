a22354cd040eca715270967514cc7175617eedd8
Ranjan Poudel <ranjanpoudel1234@gmail.com>
2018-06-16 09:53:04 -0500

    file1 variable in dev branch

--- HelloWorldGitHubPractice/File1.cs
+++ HelloWorldGitHubPractice/File1.cs
@@ -7,5 +7,7 @@ namespace HelloWorldGitHubPractice
     public class File1
     {
         public string File1TestVariable { get; set; }
+
+        public string DevBranchVariable { get; set; }
     }
 }
