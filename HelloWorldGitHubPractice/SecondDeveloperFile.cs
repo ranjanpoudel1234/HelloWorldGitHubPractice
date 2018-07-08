1e42afd7087531decdcdc927eb3a234691ffea01
Ranjan Poudel <ranjan@pecsafety.com>
2018-07-08 13:42:51 -0500

    second developer file.cs class added

---
+++ HelloWorldGitHubPractice/SecondDeveloperFile.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldGitHubPractice
+{
+    public class SecondDeveloperFile
+    {
+        public string Name { get; set; }
+    }
+}
